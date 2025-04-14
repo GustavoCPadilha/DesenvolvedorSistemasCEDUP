70f583bbc5348ebff3fc2d5029bbb0b194a9c380
GustavoCPadilha <padilhacgustavo@gmail.com>
2025-04-14 20:55:26 -0300

    Create exibirMatrizes.java

---
+++ Java/exibirMatrizes.java
@@ -0,0 +1,49 @@
+package com.mycompany.gustavo;
+
+
+public class Gustavo
+{
+    public static void main(String[] args) 
+    {
+        char matriz_alfabeto[][] = {{'a','b','c','d','e'},{'f','g','h','i','j'}, 
+            {'l','m','n','o','p'}, {'q','r','s','t','u'}};
+        int matriz_int[][] = {{19,25,100,99},{10,7,25,14},{35,2,47,74}};
+        double matriz_float[][] = {{1.9,2.5,10.0},{1.0,7.8,2.5},{3.5,2.2,4.7}};
+        
+        exibeMatrizChar(matriz_alfabeto);
+        System.out.println();
+        exibeMatrizInt(matriz_int);
+        System.out.println();
+        exibeMatrizFloat(matriz_float);
+    }
+    
+    public static void exibeMatrizChar(char[][] matriz)
+    {
+        for (char[] matriz1 : matriz) {
+            for (int x = 0; x <= matriz.length; x++) {
+                System.out.print(" " + matriz1[x]);
+            }
+            System.out.println();
+        }
+    }
+    
+    public static void exibeMatrizInt(int[][] matriz)
+    {
+        for (int[] matriz1 : matriz) {
+            for (int x = 0; x < matriz.length; x++) {
+                System.out.print(" " + (matriz1[x]));
+            }
+            System.out.println();
+        }
+    }
+        
+    public static void exibeMatrizFloat(double[][] matriz)
+    {
+        for (double[] matriz1 : matriz) {
+            for (int x = 0; x < matriz.length; x++) {
+                System.out.print(" " + (matriz1[x]));
+            }
+            System.out.println();
+        }
+    }
+}
