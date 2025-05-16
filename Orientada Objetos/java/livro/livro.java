560cfa1bd11c9bc41dec405bf2e4712f2ff76b67
GustavoCPadilha <padilhacgustavo@gmail.com>
2025-05-16 20:15:29 -0300

    Create livro.java

---
+++ Orientada Objetos/java/livro/livro.java
@@ -0,0 +1,47 @@
+package com.mycompany.ex03;
+
+
+public class Livro {
+    String titulo;
+    String autor;
+    int paginas;
+    boolean disponivel;
+    
+    void emprestar()
+    {
+        if (disponivel == true)
+        {
+            System.out.println("Livro emprestado com sucesso!");
+            this.disponivel = false;
+        }
+        else
+        {
+            System.out.println("ERRO! Livro indisponível!");
+        }
+    }
+    
+    void devolver()
+    {
+        if (disponivel == true)
+        {
+            System.out.println("ERRO! O livro não foi emprestado para ser devolvido!");
+        }
+        else
+        {
+            System.out.println("Livro devolvido com sucesso!");
+            this.disponivel = true;
+        }
+    }
+    
+    void verificar()
+    {
+        if (disponivel == true)
+        {
+            System.out.println("Livro disponível!");
+        }
+        else
+        {
+            System.out.println("Livro indisponível!");
+        }
+    }
+}
