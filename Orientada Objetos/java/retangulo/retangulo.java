4ca6dfd5ccf94f13677f837ee458bc272a655ed5
GustavoCPadilha <padilhacgustavo@gmail.com>
2025-05-16 20:26:32 -0300

    Create retangulo.java

---
+++ Orientada Objetos/java/retangulo/retangulo.java
@@ -0,0 +1,17 @@
+package com.mycompany.ex04;
+
+
+public class Retangulo {
+    float largura;
+    float altura;
+    
+    float area()
+    {
+        return largura * altura;
+    }
+    
+    float perimetro()
+    {
+        return largura * 2 + altura * 2;
+    }
+}
