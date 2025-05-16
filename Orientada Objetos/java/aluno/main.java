6421dfc52c92444381d5046d2274c664022c7091
GustavoCPadilha <padilhacgustavo@gmail.com>
2025-05-16 19:34:28 -0300

    Create main.java

---
+++ Orientada Objetos/java/aluno/main.java
@@ -0,0 +1,27 @@
+package com.mycompany.aluno;
+
+import java.util.Scanner;
+
+public class Ex01 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Aluno aluno = new Aluno();
+        aluno.nome = "Gustavo";
+        aluno.matricula = "4540667075";
+        
+        System.out.print("Digite a quantidade de notas do aluno: ");
+        int qtde_notas = input.nextInt();
+        aluno.notas = new float[qtde_notas];
+        
+        for(int i = 0; i < qtde_notas; i++)
+        {
+            System.out.format("Digite a nota %d: ", i+1);
+            aluno.notas[i] = input.nextFloat();
+        }
+        
+        float media = aluno.media();
+        System.out.format("A média do aluno é de: %.2f", media);
+        aluno.situacao();
+    }
+}
