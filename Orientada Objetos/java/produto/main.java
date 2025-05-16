dd93666775226b2e984f987ab7c65709f8a6ae53
GustavoCPadilha <padilhacgustavo@gmail.com>
2025-05-16 19:46:29 -0300

    Create main.java

---
+++ Orientada Objetos/java/produto/main.java
@@ -0,0 +1,16 @@
+package com.mycompany.produto;
+
+
+public class Ex02 {
+
+    public static void main(String[] args) {
+        Produto chiclete = new Produto();
+        chiclete.nome = "Trident";
+        chiclete.preco = 2.50f;
+        chiclete.qtde = 7;
+        
+        float valorEstoque = chiclete.valorEstoque();
+        System.out.format("Valor total em estoque: R$%.2f\n", valorEstoque);
+        chiclete.disponivel();
+    }
+}
