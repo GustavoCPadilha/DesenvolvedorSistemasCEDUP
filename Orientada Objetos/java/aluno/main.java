fe2f058e54665b69fb4c03af5c8336bd1d1c0cdb
GustavoCPadilha <padilhacgustavo@gmail.com>
2025-05-16 20:17:59 -0300

    Update main.java

--- Orientada Objetos/java/aluno/main.java
+++ Orientada Objetos/java/aluno/main.java
@@ -1,3 +1,11 @@
+/*
+1) Implemente uma classe chamada “Aluno” que possua atributos para
+armazenar o nome, a matrícula e as notas de um aluno. Adicione
+métodos para calcular a média das notas e verificar a situação do
+aluno (aprovado ou reprovado);
+*/
+
+
 package com.mycompany.aluno;
 
 import java.util.Scanner;
